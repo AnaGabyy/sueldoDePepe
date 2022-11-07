a32c3e1f09a384d3626eb26f9e64a97c6233e5ae
Ana Medina <112995260+AnaGabyy@users.noreply.github.com>
2022-11-07 12:26:53 -0300

    feat: add employee interface

---
+++ src/IEmpleado.java
@@ -0,0 +1,9 @@
+public interface IEmpleado {
+
+    double calcularSueldoNeto();
+    double calcularBonoPorPresentismo(int diasAusente);
+    double calcularBonoPorResultados();
+    double calcularSueldoTotal(int diasAusente);
+
+}
+
