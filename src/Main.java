0818bd7808f70ece8f06b1a02be5ea46c6c4e3a1
Ana Medina <112995260+AnaGabyy@users.noreply.github.com>
2022-11-07 12:38:11 -0300

    feat: implements cadete class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sueldo de Pepe");
 
+        Cadete pepe = new Cadete();
+        System.out.println(String.format("El sueldo total de el empleado es %f",
+                pepe.calcularSueldoTotal(0)
+                ));
     }
 }
